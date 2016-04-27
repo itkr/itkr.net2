8567a7d73ace843a6904cecdb9e29e16d7537443
itkr <itkrst@gmail.com>
2016-04-28 00:04:21 +0900

    Fix wsgi

--- wsgi/production.py
+++ wsgi/production.py
@@ -8,10 +8,12 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.production")
 
